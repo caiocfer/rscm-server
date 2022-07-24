f998b13ea05ed840bbc1dd8c84b991aa0e6f140d
Caio <caiocfer@gmail.com>
2022-07-24 18:32:58 -0300

    Add User model
    Added the User model and it's function for creating a new User
    
    Signed-off-by: Caio <caiocfer@gmail.com>

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 )
---
+++ src/models/User.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"errors"
+	"rscm/src/security"
+	"strings"
+)
+
+type User struct {
+	User_id  uint64 `json:"user_id,omitempty"`
+	Username string `json:"username,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func (user *User) validateFields() error {
+	if user.Username == "" {
+		return errors.New("Username can't be empty")
+	}
+
+	if user.Name == "" {
+		return errors.New("Name can't be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email can't be empty")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password can't be empty")
+	}
+
+	return nil
+}
+
+func (user *User) formatFiles() {
+	user.Username = strings.TrimSpace(user.Username)
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Password = user.HashPassword(user.Password)
+
+}
+
+func (user *User) HashPassword(password string) (hashedPassword string) {
+	hashPassword, error := security.HashPassword(user.Password)
+	if error != nil {
+		return string(error.Error())
+	}
+	user.Password = string(hashPassword)
+
+	return hashedPassword
+
+}
