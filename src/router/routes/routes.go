09ec485d18fb64ccdf17d4f6327efc89f6626b4b
Caio <caiocfer@gmail.com>
2022-09-09 02:15:43 -0300

    Fixed create user bug
    This commit fixes a bug that was preventing user from creating a new
    account.
    
    Signed-off-by: Caio <caiocfer@gmail.com>

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -22,7 +22,8 @@ func ConfigRoutes(r *mux.Router) *mux.Router {
 
 	for _, route := range routes {
 		if route.RequerisAuthentication {
-			r.HandleFunc(route.URI, auth.AuthUser(route.Function))
+			//r.HandleFunc(route.URI, middleware.Logger(auth.AuthUser(route.Function))).Methods(route.Method)
+			r.HandleFunc(route.URI, auth.AuthUser(route.Function)).Methods(route.Method)
 		} else {
 			r.HandleFunc(route.URI, route.Function).Methods(route.Method)
 
